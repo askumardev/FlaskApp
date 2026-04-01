330c31f8197a19bd38f9376117dd298075691075
askumardev <askumar.dev@outlook.com>
2026-04-01 17:55:47 +0530

    main

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def hello_world():
+    return render_template("index.html")
 
-@app.route("/services")
-def services():
-    return render_template("services.html")
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# @app.route("/services")
+# def services():
+#     return render_template("services.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
 
-app.run(debug=True)
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
+
+# app.run(debug=True)
+app.run(port=8000, debug=True)
